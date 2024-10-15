dc4e8bd647d4f722b475f911ca45c26dc7036b6d
elshodibadullayev28@gmail.com <elshodibadullayev28@gmail.com>
2024-10-15 19:19:26 +0100

    INFRA:Initialize API Project

---
+++ Project.Api/Startup.cs
@@ -0,0 +1,57 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+// Free To use Comfort and peace
+//=================================================
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+namespace Project.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+               Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apiInfo = new OpenApiInfo
+            {
+                Title="Project.Api",
+                Version="v1"
+            };
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: apiInfo);
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json",
+                    name: "Project.Api v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            endpoints.MapControllers());
+        }
+    }
+}
