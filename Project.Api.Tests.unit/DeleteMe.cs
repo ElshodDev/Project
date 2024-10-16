e595f699b531b35d030170cf46feb823ff28c1b4
elshodibadullayev28@gmail.com <elshodibadullayev28@gmail.com>
2024-10-16 16:34:23 +0100

    INFRA:Initialize Unit Test Project

---
+++ Project.Api.Tests.unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+// Free To use Comfort and peace
+//=================================================
+
+using Xunit;
+
+namespace Project.Api.Tests.unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=>Assert.True(true);
+    }
+}
