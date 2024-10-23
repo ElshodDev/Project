e9b78d10036421ca5043e92539982b36f91afa1b
elshodibadullayev28@gmail.com <elshodibadullayev28@gmail.com>
2024-10-23 19:00:12 +0100

    BROKERS: Insert Guest

---
+++ Project.Api/Brokers/IStorageBroker.Guests.cs
@@ -0,0 +1,13 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+// Free To use Comfort and peace
+//=================================================
+using Project.Api.Models.Foundations.Guests;
+
+namespace Project.Api.Brokers
+{
+    public partial  interface IStorageBroker
+    {
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
+    }
+}
--- Project.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Project.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -3,6 +3,7 @@
 // Free To use Comfort and peace
 //=================================================
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Project.Api.Models.Foundations.Guests;
 
 namespace Project.Api.Brokers.Storages
@@ -10,5 +11,16 @@ namespace Project.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Guest> Guests { get; set; }
+
+      public  async ValueTask<Guest> InsertGuestAsync(Guest guest)
+        {
+            using var broker=new StorageBroker(this.configuration);
+
+            EntityEntry<Guest> guestEntityEnty = await broker.Guests.AddAsync(guest);
+
+            await broker.SaveChangesAsync();
+
+            return guestEntityEnty.Entity;
+        }
     }
 }
